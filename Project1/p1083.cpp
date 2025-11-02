938003c14d0fcd14ad6f3bb3e36abecb06af185b
luomo233 <157657883@qq.com>
2025-11-02 20:37:14 +0800

    final

---
+++ Project1/p1083.cpp
@@ -0,0 +1,21 @@
+﻿//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int n = 0;
+//	int m = 0;
+//	cin >> n >> m;
+//	vector <long long> classes(n,0);
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> classes[i];
+//	}
+//
+//
+//
+//	return 0;
+//}
---
+++ Project1/p2882.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	long long n = 0;
+	long long k = INT_MAX;
+	cin >> n;
+	vector <char> front(n+1, 0);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> front[i];
+	}
+
+	long long count = 0;
+	for (int i = 0; i < n;)
+	{
+		long long temp_k = 0;
+		while (front[i] == 'B')
+		{
+			front[i] = 'F';
+			temp_k++;
+			if (front[i + 1] == 'F')
+			{
+				front[i + 1] = 'B';
+				temp_k++;
+				break;
+			}
+			i++;
+		}
+		k = min(k, temp_k);
+		count++;
+		while (front[i] == 'F')
+		{
+			i++;
+		}
+
+	}
+
+	std::cout << k << ' ' << count << endl;
+
+	return 0;
+}
